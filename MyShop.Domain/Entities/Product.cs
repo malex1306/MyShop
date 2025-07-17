1eb85c0b4979509f5bee1a3f8a823ce0aff963dd
malex1306 <alexandremarcel350@gmail.com>
2025-07-17 08:43:35 +0200

    add: Product Entities and ValueObjects

--- MyShop.Domain/Entities/Product.cs
+++ MyShop.Domain/Entities/Product.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyShop.Domain.ValueObjects;
 
 namespace MyShop.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public ProductName Name { get; private set; }
+        public Money Price { get; set; }
+        public string? Description { get; set; }
+        public ProductCategory Category { get; set; }
+        public int StockQuantity { get; set; }
+
+        public Product(ProductName name, Money price, string? description, ProductCategory category, int stockQuantity)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Price = price ?? throw new ArgumentNullException(nameof(price));
+            Description = description;
+            Category = category ?? throw new ArgumentNullException(nameof(category));
+            StockQuantity = stockQuantity >= 0 ? stockQuantity : throw new ArgumentOutOfRangeException(nameof(stockQuantity), "Stock quantity cannot be negative.");
+        }
+
+        public void DescreaseStock(int quantity)
+        {
+            if (quantity > StockQuantity)
+            {
+                throw new InvalidOperationException("Nicht genug Lagerbestand.");
+
+                StockQuantity -= quantity;
+            }
+        }
     }
 }
