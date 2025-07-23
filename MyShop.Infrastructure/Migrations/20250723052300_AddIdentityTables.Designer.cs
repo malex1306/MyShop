2476e90b5d6c641273d0256017c0753275718b7c
malex1306 <alexandremarcel350@gmail.com>
2025-07-23 07:24:43 +0200

    add: Customer migration

--- MyShop.Infrastructure/Migrations/20250723052300_AddIdentityTables.Designer.cs
+++ MyShop.Infrastructure/Migrations/20250723052300_AddIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyShop.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using MyShop.Infrastructure.Persistence;
 namespace MyShop.Infrastructure.Migrations
 {
     [DbContext(typeof(MyShopDbContext))]
-    partial class MyShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250723052300_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
